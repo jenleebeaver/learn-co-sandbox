96c43f810679ca5782b1ab72b738f59cbf7076c2
Jennifer Beaver <jen.lee.beaver@gmail.com>
2021-03-05 08:59:02 +0000

    Automatically backed up by Learn

---
+++ conditionalstatements/index.js
@@ -0,0 +1,41 @@
+if (5 > 2) {
+  console.log("5 is greater than 2")
+}
+if (2>5) {
+   console.log("2 is greater than 5")
+   }
+   if ( 6+3 === 9) {
+   console.log("Giraffes have no vocal cords.")
+   }
+   var dog = "satisfied"
+   
+   if (dog === "hungry") {
+     console.log("Refilling food bowl.")
+   } else {
+     console.log("Reading newspaper.")
+   }
+   
+   var dog = "thirsty"
+   
+   if (dog === "hungry") {
+     console.log("Refilling food bowl.")
+     } else if (dog === "thirsty") {
+       console.log("Refilling water bowl.")
+     } else {
+       console.log("Reading newspaper.")
+     }
+   
+   var dog = "cuddly"
+      
+    if (dog === "hungry") {
+      console.log("Refilling food bowl.")
+    } else if (dog === "thirsty") {
+      console.log("Refilling water bowl.")
+    } else if (dog === "playful") {
+      console.log("Playing tug-of-war.")
+    } else if (dog === "cuddly") {
+      console.log("Snuggling.")
+    } else {
+      console.log("Reading newspaper.")
+    }
+    
---
+++ functionindex.js
@@ -0,0 +1,34 @@
+
+function sayHelloToSofia () {
+  console.log("Hello, Sofia!")
+}
+
+sayHelloToSofia ()
+
+function sayHelloToBrendan () {
+  console.log("Hello, Brendan!")
+}
+
+sayHelloToBrendan ()
+
+function sayHelloToIsabel () {
+  console.log("Hello, Isabel!")
+}
+
+sayHelloToIsabel ()
+
+function doSomething(thing) {
+  console.log(thing)
+}
+
+doSomething('anything')
+
+function
+sayHelloTo(firstName) {
+  console.log('Hello,${firstName}!')
+}
+
+sayHelloTo('Isabel')
+sayHelloTo('Jane')
+sayHelloTo('R2-D2')
+sayHelloTo( 1 )
